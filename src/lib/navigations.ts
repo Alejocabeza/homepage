ceb0301913552f1b08e4a5be9f73397d7dc07a28
Alejandro Cabeza <alejandroworkoficial@gmail.com>
2024-05-09 16:49:28 -0400

    new portfolio

---
+++ src/lib/navigations.ts
@@ -0,0 +1,22 @@
+export const NAVIGATION = [
+  {
+    title: "Inicio",
+    url: "/",
+    icon: null,
+  },
+  {
+    title: "Proyectos",
+    url: "/projects",
+    icon: null,
+  },
+  {
+    title: "CV",
+    url: "/curriculum-vitae",
+    icon: null,
+  },
+  // {
+  //   title: "Blog",
+  //   url: "/blog",
+  //   icon: null,
+  // },
+];
