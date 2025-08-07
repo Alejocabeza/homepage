d2fec9077185e3742b0cdf6d2fec9c138befdb0d
Alejandro Cabeza <alejandrocabeza400@gmail.com>
2025-08-07 19:07:54 -0400

    fix pallet color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,15 @@ export default {
 	darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#0A192F',
+				'accent': '#64FFDA',
+				'text-primary': '#CCD6F6',
+				'text-secondary': '#8892B0',
+				'bg-secondary': 'rgb(2 12 27 / <alpha-value>)',
+			}
+		},
 	},
 	plugins: [],
 }
